a29be684dd493fe760b2c055f22d2053b586180e
Max Lapan <max.lapan@gmail.com>
2010-03-07 21:10:30 +0300

    Display region and it's name in widget

--- mainwidget.hpp
+++ mainwidget.hpp
@@ -18,15 +18,31 @@ public:
     MainWidget ()
         : QLabel ()
     {
+        const RegionInfo *ri;
+
         setAlignment(Qt::AlignCenter);
         setAttribute(Qt::WA_TranslucentBackground);
 
-        setText (_settings.regions ().join (", "));
+        ri = _regions.lookup (_settings.regions ()[0]);
+
+        setText (_settings.regions ().join (", ") + ", " + QString (ri ? ri->name () : ""));
     }
 
     QSize sizeHint() const
     {
         return 2 * QLabel::sizeHint();
+    }
+
+protected:
+    void paintEvent(QPaintEvent *event)
+    {
+        QPainter p(this);
+        p.setBrush(QColor(0, 0, 0, 128));
+        p.setPen(Qt::NoPen);
+        p.drawRoundedRect(rect(), 25, 25);
+        p.end();
+
+        QLabel::paintEvent(event);
     }
 };
 
