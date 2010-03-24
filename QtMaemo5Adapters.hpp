38fabbffff1b2218cc868eca3cca6a493616c51f
Max Lapan <max.lapan@gmail.com>
2010-03-24 17:26:07 +0300

    Add extra field for value button adapter.

--- QtMaemo5Adapters.hpp
+++ QtMaemo5Adapters.hpp
@@ -7,9 +7,19 @@
 class QMaemo5ValueButton : public QPushButton
 {
     Q_OBJECT
+public:
+    enum Layout {
+        ValueUnderText,
+    };
 private:
 public:
     QMaemo5ValueButton (const QString& text, QWidget *parent);
+
+    void setValueLayout (Layout)
+    {};
+
+    void setValueText (const QString &)
+    {};
 };
 
 
